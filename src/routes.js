4e169261691e0e66baaf1a123cb4efef1b70556b
vwvwMM <vincentwu0628@gmail.com>
2021-07-17 23:17:37 +0800

    put model inside

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Column = React.lazy(() => import('./views/components/column/Column'))
+const ColumnSummary = React.lazy(() => import('./views/components/columnSummary/columnSummary'))
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/column', name: 'Column', component: Column },
+  { path: '/columnSummary', name: 'ColumnSummary', component: ColumnSummary },
 ]
 
 export default routes
