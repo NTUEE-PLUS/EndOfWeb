119fb9cff27606c0989a5834c222ad79823594fe
vwvwMM <vincentwu0628@gmail.com>
2021-07-18 00:48:14 +0800

    one article one row

--- src/views/components/columnSummary/columnSummary.js
+++ src/views/components/columnSummary/columnSummary.js
@@ -59,21 +59,20 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
   },
 }))
-
 function ColumnSummary() {
   const classes = useStyles()
 
   return (
-    <div className="App">
+    <div className="gallery">
       <Box className={classes.hero}>
         <Box>All Release</Box>
       </Box>
-      <Container maxWidth="lg" className={classes.blogsContainer}>
+      <div className={classes.blogsContainer}>
         <Typography variant="h4" className={classes.blogTitle}>
           Articles
         </Typography>
         <Grid container spacing={1}>
-          <Grid item xs={12} sm={12} md={12}>
+          <Grid item xs={12} md={12}>
             <Card className={classes.card}>
               <CardActionArea>
                 <CardMedia
@@ -109,7 +108,7 @@ function ColumnSummary() {
               </CardActions>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={12} md={12}>
+          <Grid item xs={12} md={12}>
             <Card className={classes.card}>
               <CardActionArea>
                 <CardMedia
@@ -145,83 +144,11 @@ function ColumnSummary() {
               </CardActions>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    React useContext
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-                    ranging across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.cardActions}>
-                <Box className={classes.author}>
-                  <Avatar src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-                  <Box ml={2}>
-                    <Typography variant="subtitle2" component="p">
-                      Guy Clemons
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary" component="p">
-                      May 14, 2020
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <BookmarkBorderIcon />
-                </Box>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    React useContext
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-                    ranging across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.cardActions}>
-                <Box className={classes.author}>
-                  <Avatar src="https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80" />
-                  <Box ml={2}>
-                    <Typography variant="subtitle2" component="p">
-                      Guy Clemons
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary" component="p">
-                      May 14, 2020
-                    </Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <BookmarkBorderIcon />
-                </Box>
-              </CardActions>
-            </Card>
-          </Grid>
         </Grid>
         <Box my={4} className={classes.paginationContainer}>
           <Pagination count={10} />
         </Box>
-      </Container>
+      </div>
     </div>
   )
 }
