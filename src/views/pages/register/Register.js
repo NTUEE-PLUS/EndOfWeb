417c8b9b7f72296b1b4de932293fdcd42db16d90
noidname01 <tim983649@gmail.com>
2021-07-18 18:31:01 +0800

    add RegisterEntry and Register Page

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -9,30 +9,36 @@ import {
   CFormControl,
   CInputGroup,
   CInputGroupText,
+  CListGroup,
+  CListGroupItem,
+  CFormLabel,
+  CTooltip,
   CRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
 const Register = () => {
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+    <div className="min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md="9" lg="7" xl="6">
+        <CRow>
+          <CCol md="11" lg="8" xl="7">
             <CCard className="mx-4">
               <CCardBody className="p-4">
                 <CForm>
-                  <h1>Register</h1>
+                  <h1>Just A Few Steps to Join EE+!</h1>
                   <p className="text-medium-emphasis">Create your account</p>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
                       <CIcon name="cil-user" />
                     </CInputGroupText>
-                    <CFormControl placeholder="Username" autoComplete="username" />
+                    <CFormControl placeholder="Your Chinese Name" />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
-                    <CInputGroupText>@</CInputGroupText>
-                    <CFormControl placeholder="Email" autoComplete="email" />
+                    <CInputGroupText>
+                      <CIcon name="cil-education" />
+                    </CInputGroupText>
+                    <CFormControl placeholder="Student ID" />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -54,12 +60,38 @@ const Register = () => {
                       autoComplete="new-password"
                     />
                   </CInputGroup>
-                  <CButton color="success" block>
-                    Create Account
-                  </CButton>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormControl placeholder="Email" autoComplete="email" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon name="cil-image" />
+                    </CInputGroupText>
+                    <CFormLabel htmlFor="formFile">ID Photo</CFormLabel>
+                    <CTooltip
+                      content="  ID photo is used to confirm your
+                                        identity, and will be auto deleted after
+                                        account is activated"
+                    >
+                      <CFormControl id="formFile" type="file"></CFormControl>
+                    </CTooltip>
+                  </CInputGroup>
+                  <CRow className="justify-content-center">
+                    <div className="d-flex justify-content-center">
+                      <CButton color="dark" block>
+                        Create Account
+                      </CButton>
+                    </div>
+                  </CRow>
                 </CForm>
               </CCardBody>
             </CCard>
+          </CCol>
+          <CCol className="ml-3" style={{ borderLeft: '1px solid gray' }}>
+            {/* <CListGroup>
+              <CListGroupItem>XDXDXDXDXDXDXDX</CListGroupItem>
+            </CListGroup> */}
           </CCol>
         </CRow>
       </CContainer>
