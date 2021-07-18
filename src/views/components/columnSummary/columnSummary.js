245891d596fb8e11dc033b4c5475d7ccb7e5dc1a
vwvwMM <vincentwu0628@gmail.com>
2021-07-18 15:56:20 +0800

    split anno in db and show

--- src/views/components/columnSummary/columnSummary.js
+++ src/views/components/columnSummary/columnSummary.js
@@ -56,6 +56,21 @@ const useStyles = makeStyles((theme) => ({
 
 const ColumnSummary = ({ data }) => {
   const classes = useStyles()
+  const contributions = (person) => {
+    return (
+      <Box className={classes.author}>
+        <Avatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+        <Box ml={2}>
+          <Typography variant="subtitle2" component="p">
+            {person}
+          </Typography>
+          <Typography variant="subtitle2" color="textSecondary" component="p">
+            May 14, 2020
+          </Typography>
+        </Box>
+      </Box>
+    )
+  }
   const articles = data.map((art) => {
     return (
       <Grid item xs={12} md={12} key={art.key}>
@@ -76,17 +91,9 @@ const ColumnSummary = ({ data }) => {
             </CardContent>
           </CardActionArea>
           <CardActions className={classes.cardActions}>
-            <Box className={classes.author}>
-              <Avatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-              <Box ml={2}>
-                <Typography variant="subtitle2" component="p">
-                  Guy Clemons
-                </Typography>
-                <Typography variant="subtitle2" color="textSecondary" component="p">
-                  May 14, 2020
-                </Typography>
-              </Box>
-            </Box>
+            {art.anno.map((person) => {
+              return contributions(person)
+            })}
             <Box>
               <BookmarkBorderIcon />
             </Box>
@@ -99,7 +106,7 @@ const ColumnSummary = ({ data }) => {
     <div>
       <introTitle />
       <Box className={classes.hero}>
-        <Box>All Release {classes.hero}</Box>
+        <Box>All Release</Box>
       </Box>
       <div className={classes.blogsContainer}>
         <Typography variant="h4" className={classes.blogTitle}>
