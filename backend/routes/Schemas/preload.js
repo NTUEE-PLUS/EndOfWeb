557640fd13015a7c3e762c5133b96fc26f07b680
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-08-18 12:09:16 +0800

    preload script added

--- backend/routes/Schemas/preload.js
+++ backend/routes/Schemas/preload.js
@@ -1,35 +1,32 @@
-// const mongoose = require('mongoose');
-// const userSchema = new mongoose.Schema({
-// 	filename:{type:String},
-//     columnImg: {//column 的照片
-// 	  data:{type:Buffer},
-// 	  contentType:{type:String}
-// 	}
-// })
-// const sourceUrl = 'mongodb+srv://ntueeplus:ntueeplus2020@cluster0.fctiy.mongodb.net/heroku_kbtrwz4h?retryWrites=true&w=majority',
-//     targetUrl = process.env.MONGO_URI||'mongodb+srv://ntueeplus:ntueeplus2020@cluster0.gbnte.mongodb.net/EEplus?retryWrites=true&w=majority',
-//     collectionName = 'columns';
+const mongoose = require('mongoose')
+const { Schema: columnDetail, collectionName: detailName } = require('./column_detail')
+const { Schema: columnOutline, collectionName: outlineName } = require('./column_outline')
+const sourceUrl =
+    'mongodb+srv://ntueeplus:ntueeplus2020@cluster0.fctiy.mongodb.net/heroku_kbtrwz4h?retryWrites=true&w=majority',
+  targetUrl = process.env.MONGO_URI
 
+const copy = async (sourceDB, targetDB, schema, name, async = false) => {
+  const sourceModel = sourceDB.model(name, schema)
+  const targetModel = targetDB.model(name, schema)
+  const docs = await sourceModel.find()
+  await targetModel.insertMany(docs, { ordered: async }) //ordered true may run faster(?) but will be unordered
+}
 
-
-
-
-// (async ()=>{
-//     const sourceDB = await mongoose.createConnection(sourceUrl);
-//     const sourceModel = sourceDB.model(collectionName,userSchema);
-//     const targetDB = await mongoose.createConnection(targetUrl);
-//     const targetModel = targetDB.model(collectionName,userSchema);
-//     targetDB.db.dropDatabase(function (err, res) {
-//         if (err) return console.log(err);
-//         console.log('docker DB dropped');
-//     })
-//     const docs = await sourceModel.find({});
-//     for(let doc of docs){
-//         //console.log(doc);
-//         await new targetModel({
-//             filename:doc.filename,
-//             columnImg:doc.columnImg
-//         }).save();
-//     }
-//     mongoose.disconnect();
-// })();
+const main = async () => {
+  if (!targetUrl) throw new Error('MONGO_URI not set in env')
+  const sourceDB = await mongoose.createConnection(sourceUrl)
+  const targetDB = await mongoose.createConnection(targetUrl)
+  targetDB.db.dropDatabase(function (err, res) {
+    if (err) return console.log(err)
+    console.log('docker DB dropped')
+  })
+  const task1 = async () => {
+    await copy(sourceDB, targetDB, columnDetail, detailName)
+  }
+  const task2 = async () => {
+    await copy(sourceDB, targetDB, columnOutline, outlineName)
+  }
+  await Promise.all([task1(), task2()])
+  mongoose.disconnect()
+}
+main()
