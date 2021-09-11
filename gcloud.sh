0d898b8affcf7b6a9f93e0de4789c2f3855beec8
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-09-11 23:41:52 +0800

    gcloud conf

--- gcloud.sh
+++ gcloud.sh
@@ -1,3 +1,4 @@
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 gcloud builds submit --tag gcr.io/$PROJECT_ID/eeplusweb
-gcloud run deploy --image gcr.io/$PROJECT_ID/eeplusweb
+gcloud config set run/region asia-east1
+gcloud run deploy eeplus --image gcr.io/$PROJECT_ID/eeplusweb
