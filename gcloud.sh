a4eabcdaabf5761f85be457e32d209e3911224b8
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-09-12 00:08:24 +0800

    auto deploy test

--- gcloud.sh
+++
@@ -1,4 +0,0 @@
-export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
-gcloud builds submit --tag gcr.io/$PROJECT_ID/eeplusweb
-gcloud config set run/region asia-east1
-gcloud run deploy eeplus --image gcr.io/$PROJECT_ID/eeplusweb
