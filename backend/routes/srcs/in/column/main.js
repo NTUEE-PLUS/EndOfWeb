0cc9e5f9cc419323d50024bfcdaae70100759fd1
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-09-15 17:46:40 +0800

    api done

--- backend/routes/srcs/in/column/main.js
+++ backend/routes/srcs/in/column/main.js
@@ -15,6 +15,8 @@ router.get('/detail', require('./getDetail'))
 router.get('/outline', require('./getOutline'))
 
 router_auth.post('/add', parseFile('file'), require('./addColumn'))
+router_auth.post('/update', parseFile('file'), require('./update'))
+router_auth.post('/delete', require('./delete'))
 
 router.get('/search', require('./search'))
 
