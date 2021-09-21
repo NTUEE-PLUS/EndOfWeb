a0eb692d946fbcd994f828de9d28b6886cf197b1
noidname01 <tim983649@gmail.com>
2021-09-21 16:27:04 +0800

    contact func complete

--- client/src/views/out/home/contact/Contact.js
+++ client/src/views/out/home/contact/Contact.js
@@ -3,7 +3,7 @@ import { CCol, CContainer, CRow } from '@coreui/react'
 import React, { useState } from 'react'
 import CIcon from '@coreui/icons-react'
 
-import emailjs from 'emailjs-com'
+import axios from 'axios'
 
 const initialState = {
   name: '',
@@ -22,15 +22,23 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(name, email, message)
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID').then(
-      (result) => {
-        console.log(result.text)
+
+    const url =
+      'https://docs.google.com/forms/d/e/1FAIpQLSed-GXXBhqIRUBEX7-nMlwuQ3a22-Z51mtxVSlcyhWzG9TH2Q/formResponse'
+
+    axios
+      .get(url, {
+        params: {
+          usp: 'pp_url',
+          'entry.1670134810': name,
+          'entry.302205267': email,
+          'entry.307115258': message,
+        },
+      })
+      .then((res) => {
+        alert('感謝您的意見!')
         clearState()
-      },
-      (error) => {
-        console.log(error.text)
-      },
-    )
+      })
   }
   return (
     <div>
@@ -58,6 +66,7 @@ const Contact = () => {
                           placeholder="Name"
                           required
                           onChange={handleChange}
+                          value={name}
                         />
                         <p className="help-block text-danger"></p>
                       </div>
@@ -72,6 +81,7 @@ const Contact = () => {
                           placeholder="Email"
                           required
                           onChange={handleChange}
+                          value={email}
                         />
                         <p className="help-block text-danger"></p>
                       </div>
@@ -86,6 +96,7 @@ const Contact = () => {
                       placeholder="Message"
                       required
                       onChange={handleChange}
+                      value={message}
                     ></textarea>
                     <p className="help-block text-danger"></p>
                   </div>
