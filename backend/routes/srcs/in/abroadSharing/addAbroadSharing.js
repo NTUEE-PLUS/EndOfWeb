1168cdf8e719c7e35437490a5d1aa2c1bcd5bf86
weipo <b10901053@ntu.edu.tw>
2023-02-13 11:46:48 +0800

    imgadd

--- backend/routes/srcs/in/abroadSharing/addAbroadSharing.js
+++ backend/routes/srcs/in/abroadSharing/addAbroadSharing.js
@@ -1,6 +1,7 @@
 const { dbCatch } = require('../../../error')
 const AbroadSharing = require('../../../Schemas/abroad_sharing')
 const asyncHandler = require('express-async-handler')
+const { parseImg } = require('../../../Schemas/query')
 
 /**
  * @api {post} /addAbroadSharing add abroadsharing
@@ -8,20 +9,28 @@ const asyncHandler = require('express-async-handler')
  * @apiGroup In/abroadSharing
  * @apiDescription 新增留學分享
  *
+ * @apiParam {String} title 採訪標題
  * @apiParam {String} intro 介紹
- * @apiParam {String} YTlink Youtube連結
- * @apiParam {String} otherLinks 其他外部連結
+ * @apiParam {URL} YTlink Youtube連結
+ * @apiParam {URL} otherLinks 其他外部連結
+ * @apiParam {File} file 留學分享照片
  *
  * @apiSuccess (201) {String} _id _id
  *
  * @apiError (500) {String} description 資料庫錯誤
  */
 const addAbroadSharing = async (req, res) => {
-  const { intro, YTlink, otherLinks } = req.body
-  const { _id } = await new AbroadSharing({ intro, YTlink, otherLinks }).save().catch(dbCatch)
+  const { title, intro, YTlink, otherLinks } = req.body
+  const img = parseImg(req.file)
+  const { _id } = await new AbroadSharing({ title, intro, YTlink, otherLinks, img })
+    .save()
+    .catch(dbCatch)
   res.status(201).send({ _id })
 }
 
 const valid = require('../../../middleware/validation')
-const rules = [{ filename: 'optional', field: ['intro', 'YTlink', 'otherLinks'], type: 'String' }]
+const rules = [
+  { filename: 'optional', field: ['title', 'intro', 'YTlink', 'otherLinks'], type: 'String' },
+]
+
 module.exports = [valid(rules), asyncHandler(addAbroadSharing)]
--- backend/routes/srcs/in/abroadSharing/main.js
+++ backend/routes/srcs/in/abroadSharing/main.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router_auth = express.Router()
 const router = express.Router()
 
-router_auth.post('/addAbroadSharing', require('./addAbroadSharing'))
 router.get('/getAbroadSharing', require('./getAbroadSharing'))
+
+router_auth.post('/addAbroadSharing', require('./addAbroadSharing'))
+
 router_auth.delete('/deleteAbroadSharing', require('./deleteAbroadSharing'))
+
 router_auth.patch('/updateAbroadSharing', require('./updateAbroadSharing'))
+
 module.exports = { router, router_auth }
