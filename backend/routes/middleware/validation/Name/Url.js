9ec7db5cd72d2dff2954472d359519f15ae2a842
weipo <b10901053@ntu.edu.tw>
2023-02-16 22:13:38 +0800

    add annotation

--- backend/routes/middleware/validation/Name/Url.js
+++ backend/routes/middleware/validation/Name/Url.js
@@ -5,6 +5,7 @@ module.exports = () => [
     nullable: true,
     checkFalsy: false,
   }),
+  // check and valid every element in the otherLinks is URL
   body('otherLinks').custom((val, { req }) => {
     function judgeURL(str) {
       const pattern = /^(https?:\/\/)?[\w.-]+\.[a-z]{2,}(\/.*)?$/
